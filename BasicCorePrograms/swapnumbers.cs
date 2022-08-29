4c47164254eb297505ac9c35653b05729310ede8
meghpatil21 <megh.after18@gmail.com>
2022-08-29 23:23:49 +0530

    [megh] Swap numbers

---
+++ BasicCorePrograms/swapnumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class swapnumbers
+    {
+        public static void SwapNo()
+        {
+            Console.WriteLine("Enter 1st number: ");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number: ");
+            int num2=Convert.ToInt32(Console.ReadLine());
+
+            int temp=num1;
+
+            num1= num2;
+            num2=temp;
+
+            Console.WriteLine("Your 1st number is: "+num1);
+            Console.WriteLine("Your 2nd number is: " + num2);
+        }
+    }
+}
