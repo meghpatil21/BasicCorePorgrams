14e3fa80bff386d242cc74b2d175b5ebb3baf329
meghpatil21 <megh.after18@gmail.com>
2022-08-29 23:39:56 +0530

    [megh] Vowel or Consonant

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose Program\n 1.Flip coin and print percentage\n 2.Leap Year\n 3.Power of 2\n 4.Harmonic No.\n 5.Factors\n 6.Quotient & Remainder\n 7.Swap Numbers\n 8.Even Odd check");
+            Console.WriteLine("Choose Program\n 1.Flip coin and print percentage\n 2.Leap Year\n 3.Power of 2\n 4.Harmonic No.\n 5.Factors\n 6.Quotient & Remainder\n 7.Swap Numbers\n 8.Even Odd check\n 9.Vowel Check");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -31,6 +31,9 @@
                     break;
                 case 8:
                     EvenOdd.OddEven();
+                    break;
+                case 9:
+                    VowelCons.VowelCon();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
---
+++ BasicCorePrograms/VowelCons.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class VowelCons
+    {
+        public static void VowelCon()
+        {
+            Console.Write("Input an Alphabet: ");
+            char ch = Convert.ToChar(Console.ReadLine().ToUpper());
+            
+                switch (ch)
+                {
+                    case 'A':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'I':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'O':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'U':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'E':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is not a vowel");
+                        break;
+                }
+            
+            
+        }
+    }
+}
