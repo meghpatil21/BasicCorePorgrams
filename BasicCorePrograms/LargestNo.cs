338bebb317f411da1e854f8acc568325587c4537
meghpatil21 <megh.after18@gmail.com>
2022-08-29 23:44:54 +0530

    [megh] Larget among 3 numbers

---
+++ BasicCorePrograms/LargestNo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LargestNo
+    {
+        public static void LargetNumber()
+        {
+            Console.WriteLine("Welcome to Three Number Comparator");
+            Console.WriteLine();
+            Console.WriteLine("Enter 1st no: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd no: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 3rd no: ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("Largest number is " + n1);
+            }
+
+            else if (n2 > n3)
+            {
+                Console.WriteLine("Largest number is " + n2);
+            }
+            else
+            {
+                Console.WriteLine("Largest number is " + n3);
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose Program\n 1.Flip coin and print percentage\n 2.Leap Year\n 3.Power of 2\n 4.Harmonic No.\n 5.Factors\n 6.Quotient & Remainder\n 7.Swap Numbers\n 8.Even Odd check\n 9.Vowel Check");
+            Console.WriteLine("Choose Program\n 1.Flip coin and print percentage\n 2.Leap Year\n 3.Power of 2\n 4.Harmonic No.\n 5.Factors\n 6.Quotient & Remainder\n 7.Swap Numbers\n 8.Even Odd check\n 9.Vowel Check\n 10.Largest number among 3 numbers");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -34,6 +34,9 @@
                     break;
                 case 9:
                     VowelCons.VowelCon();
+                    break;
+                case 10:
+                    LargestNo.LargetNumber();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
